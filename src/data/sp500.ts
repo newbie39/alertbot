fdd68d4ca4b7655a4891a156c2f321e65b9bed6a
newbie39 <airbillz23@gmail.com>
2026-04-26 17:18:07 -0400

    Add SP500 tickers to sp500.ts

---
+++ src/data/sp500.ts
@@ -0,0 +1,5 @@
+export const SP500_TICKERS = [
+  "AAPL", "MSFT", "AMZN", "GOOGL", "META", "NVDA",
+  "TSLA", "BRK.B", "UNH", "XOM", "JPM", "V", "HD",
+  "PG", "MA", "LLY", "AVGO", "CVX", "PEP", "KO"
+];
