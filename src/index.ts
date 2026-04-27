f95b16c7eab12a327c89aed64721610b7f51d672
newbie39 <airbillz23@gmail.com>
2026-04-27 12:31:55 -0400

    Register slash commands during bot initialization

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,12 @@ import dotenv from "dotenv";
 import { scheduleJobs } from "./utils/scheduler";
 import { initBotClient } from "./discord/botClient";
 
+import { registerSlashCommands } from "./discord/registerCommands";
+
+registerSlashCommands();
+initBotClient();
+scheduleJobs();
+
 dotenv.config();
 
 console.log("🚀 S&P 500 Alert Bot starting...");
